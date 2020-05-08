21c663a8ea0e007667e8eea0abe7b6186d091749
Alex <sashamur2000@mail.ru>
2020-05-08 17:11:15 +0300

    final

--- webpack.config.js
+++ webpack.config.js
@@ -7,17 +7,11 @@ module.exports = {
     path: path.join(__dirname, "/dist"),
     filename: "bundle.js"
   },
-  mode: "development",
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel", 
-        query:
-        {
-            presets:['react']
-        },
         use: ["babel-loader"]
       },
       {
